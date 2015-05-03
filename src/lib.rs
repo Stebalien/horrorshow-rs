93def086cde42c7df676a8c5901259999a949623
Steven Allen <steven@stebalien.com>
2015-05-02 21:51:10 -0400

    Document and don't depend on features.

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,129 @@
-#![feature(scoped_tls)]
+//! # Horrorshow
+//!
+//! An xml (mostly just the html subset) templating library.
+//!
+//! ## Example:
+//!
+//! ```
+//! # #[macro_use] extern crate horrorshow;
+//! # fn main() {
+//! let actual = xml! {
+//!     html {
+//!         head {
+//!             title { : "Hello world!" }
+//!         }
+//!         body {
+//!             h1(id="heading") { : "Hello!" }
+//!             p { : "Let's count to 10!" }
+//!             ol(id="count") {
+//!                 @ for i in 0..10 {
+//!                     append_xml! {
+//!                         li {
+//!                             #{"{}", i+1 }
+//!                         }
+//!                     }
+//!                 }
+//!             }
+//!             br; br;
+//!             p {
+//!                 : "Easy!"
+//!             }
+//!         }
+//!     }
+//! };
+//!
+//! let expected = "<html><head><title>Hello world!</title></head><body><h1 id=\"heading\">Hello!</h1><p>Let's count to 10!</p><ol id=\"count\"><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li><li>10</li></ol><br /><br /><p>Easy!</p></body></html>";
+//! assert_eq!(expected, actual);
+//!
+//! # }
+//! ```
+//!
+//! Or cleaned up:
+//!
+//! ```html
+//! <html>
+//!   <head>
+//!     <title>Hello world!</title>
+//!   </head>
+//!   <body>
+//!     <h1 id="heading">Hello!</h1>
+//!     <p>Let's count to 10!</p>
+//!     <ol id="count">
+//!       <li>1</li>
+//!       <li>2</li>
+//!       <li>3</li>
+//!       <li>4</li>
+//!       <li>5</li>
+//!       <li>6</li>
+//!       <li>7</li>
+//!       <li>8</li>
+//!       <li>9</li>
+//!       <li>10</li>
+//!     </ol>
+//!     <br /><br />
+//!     <p>Easy!</p>
+//!   </body>
+//! </html>
+//! ```
+//!
+//! ## Usage
+//!
+//!
+//! Inside an xml template, the following expressions are valid:
+//!
+//! * `some_tag;` -- Insert a the tag `some_tag`.
+//!
+//! * `some_tag(attr=rust_expresion,...);` -- Insert a the tag `some_tag` with the specified
+//!    attributes. The attribute values will be evaluated as rust expressions at runtime.
+//!
+//! * `some_tag { ... }` -- Insert a the tag `some_tag` and recursivly evaluate the `...`.
+//!
+//! * `some_tag(...) { ... }` -- Same as above but with custom attributes.
+//!
+//! * `: rust_expression `: { rust_code }` -- Evaluate the expression or block and insert result current position.
+//!
+//! * `#{"format_str", rust_expressions... }` -- Format the arguments according to `format_str` and insert the
+//! result at the current position.
+//!
+//! * `@ rust_expression`, `@ { rust_code }` -- Evaluate the expression or block.
+//!
+//! In rust code embedded inside of a template, you can invoke `append!("format_str", args...)` or
+//! `append_xml! { xml_template... }` to append to the template at the current position. That's how
+//! the for loop works in the example above.
+//!
+//! ## Notes:
+//!
+//! 1. This library does no escaping, sanitization. You have to do that yourself!
+//! 2. There are bugs.
 use std::cell::RefCell;
-use std::fmt::Write;
 
 // TODO: Escape?
 
-scoped_thread_local!(pub static __TEMPLATE: RefCell<String>);
+#[doc(hidden)]
+thread_local!(pub static __TEMPLATE: RefCell<Option<String>> = RefCell::new(None));
 
 #[macro_export]
 macro_rules! xml {
     ($($inner:tt)*) => {{
-        let text = RefCell::new(String::new());
-        __TEMPLATE.set(&text, || {
+        // The scoped variant is unstable
+        use ::std::cell::RefCell;
+        $crate::__TEMPLATE.with(|current| {
+            let mut stash = Some(String::new());
+            ::std::mem::swap(&mut *current.borrow_mut(), &mut stash);
             append_xml!($($inner)*);
-        });
-        text.into_inner()
+            ::std::mem::swap(&mut *current.borrow_mut(), &mut stash);
+            stash.unwrap()
+        })
     }}
 }
 
 #[macro_export]
 macro_rules! append {
     ($($tok:tt)+) => {{
-        __TEMPLATE.with(|value| {
+        use ::std::fmt::Write;
+        $crate::__TEMPLATE.with(|value| {
             // TODO: Handle errors?
-            write!(value.borrow_mut(), $($tok)+).unwrap();
+            write!(value.borrow_mut().as_mut().unwrap(), $($tok)+).unwrap();
         });
     }}
 }
@@ -82,33 +183,4 @@ macro_rules! append_xml {
         append!(concat!("<", stringify!($tag), "/>"))
     }};
     () => {""};
-}
-
-#[test]
-fn it_works() {
-    let s = xml! {
-        html {
-            head {
-                title { : "Hello world!" }
-            }
-            body {
-                h1(id="heading") { : "Hello!" }
-                p { : "Let's count to 10!" }
-                ol(id="count") {
-                    @ for i in 0..10 {
-                        append_xml! {
-                            li {
-                                #{"{}", i+1 }
-                            }
-                        }
-                    }
-                }
-                br; br;
-                p {
-                    : "Easy!"
-                }
-            }
-        }
-    };
-    println!("{}", &s);
 }
