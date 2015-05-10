ea62312c001f1dabb7588731701df79ac45b1b1b
Steven Allen <steven@stebalien.com>
2015-05-10 12:02:05 -0400

    Support namespaces and dashes.

--- src/html.rs
+++ src/html.rs
@@ -9,6 +9,13 @@ macro_rules! html {
     }}
 }
 
+#[macro_export]
+macro_rules! stringify_compressed {
+    ($($tok:tt)*) => {
+        concat!($(stringify!($tok)),*)
+    };
+}
+
 /// Append html to the current template.
 #[macro_export]
 macro_rules! append_html {
@@ -49,49 +56,49 @@ macro_rules! append_html {
         append_fmt!($($tok)+);
         append_html!($($next)*);
     }};
-    ($tag:ident($($attr:ident=$value:expr),+) { $($children:tt)* } $($next:tt)* ) => {{
-        append_raw!(concat!("<", stringify!($tag)));
+    ($($tag:ident):+($($($($attr:ident)-+):+ = $value:expr),+) { $($children:tt)* } $($next:tt)* ) => {{
+        append_raw!(concat!("<", stringify_compressed!($($tag):+)));
         $(
-            append_raw!(concat!(" ", stringify!($attr), "=\""));
+            append_raw!(concat!(" ", stringify_compressed!($($($attr)-+):+), "=\""));
             append_fmt!("{}", $value);
             append_raw!("\"");
         )+
         append_raw!(">");
         append_html!($($children)*);
-        append_raw!(concat!("</", stringify!($tag), ">"));
+        append_raw!(concat!("</", stringify_compressed!($($tag):+), ">"));
         append_html!($($next)*);
     }};
-    ($tag:ident($($attr:ident=$value:expr),+); $($next:tt)*) => {{
-        append_raw!(concat!("<", stringify!($tag)));
+    ($($tag:ident):+($($($($attr:ident)-+):+ = $value:expr),+); $($next:tt)*) => {{
+        append_raw!(concat!("<", stringify_compressed!($($tag):+)));
         $(
-            append_raw!(concat!(" ", stringify!($attr), "=\""));
+            append_raw!(concat!(" ", stringify_compressed!($($($attr)-+):+), "=\""));
             append_fmt!("{}", $value);
             append_raw!("\"");
         )+
         append_raw!(" />");
         append_html!($($next)*);
     }};
-    ($tag:ident($($attr:ident=$value:expr),+)) => {{
-        append_raw!(concat!("<", stringify!($tag)));
+    ($($tag:ident):+($($($($attr:ident)-+):+ = $value:expr),+)) => {{
+        append_raw!(concat!("<", stringify_compressed!($($tag):+)));
         $(
-            append_raw!(concat!(" ", stringify!($attr), "=\""));
+            append_raw!(concat!(" ", stringify_compressed!($($($attr)-+):+), "=\""));
             append_fmt!("{}", $value);
             append_raw!("\"");
         )+
         append_raw!(" />");
     }};
-    ($tag:ident { $($children:tt)* } $($next:tt)* ) => {{
-        append_raw!(concat!("<", stringify!($tag), ">"));
+    ($($tag:ident):+ { $($children:tt)* } $($next:tt)* ) => {{
+        append_raw!(concat!("<", stringify_compressed!($($tag):+), ">"));
         append_html!($($children)*);
-        append_raw!(concat!("</", stringify!($tag), ">"));
+        append_raw!(concat!("</", stringify_compressed!($($tag):+), ">"));
         append_html!($($next)*);
     }};
-    ($tag:ident; $($next:tt)*) => {{
-        append_raw!(concat!("<", stringify!($tag), " />"));
+    ($($tag:ident):+; $($next:tt)*) => {{
+        append_raw!(concat!("<", stringify_compressed!($($tag):+), " />"));
         append_html!($($next)*);
     }};
-    ($tag:ident) => {{
-        append_raw!(concat!("<", stringify!($tag), "/>"))
+    ($($tag:ident):+) => {{
+        append_raw!(concat!("<", stringify_compressed!($($tag):+), "/>"))
     }};
     () => {};
 }
--- tests/main.rs
+++ tests/main.rs
@@ -21,3 +21,24 @@ fn test_reentrant() {
         }
     }, "<p><a href=\"abcde\" /></p>");
 }
+
+#[test]
+fn test_namespace() {
+    assert_eq!(html! {
+        a:first {
+            b:inner;
+        }
+        c:second(e:id="testing");
+        d:last;
+    }, "<a:first><b:inner /></a:first><c:second e:id=\"testing\" /><d:last />");
+}
+
+
+#[test]
+fn test_dash() {
+    assert_eq!(html! {
+        my_tag {
+            inner(data-test="abcde");
+        }
+    }, "<my_tag><inner data-test=\"abcde\" /></my_tag>");
+}
