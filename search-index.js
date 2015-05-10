1f09d5d394df8e7ef37a2306f307913aa15714e6
Steven Allen <steven@stebalien.com>
2015-05-10 12:04:44 -0400

    Update docs for ff880563d3d8b6574cc239412457b6ad186ccdfe
    Generalize to support XML.

--- search-index.js
+++ search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['horrorshow'] = {"items":[[0,"","horrorshow","# Horrorshow",null,null],[14,"html!","","Crate a new html template",null,null],[14,"stringify_compressed!","","",null,null],[14,"append_html!","","Append html to the current template.",null,null],[14,"append_raw!","","Append text without escaping.",null,null],[14,"append_fmt!","","Format, escape, and append arguments.",null,null],[14,"append!","","Escape and append text.",null,null]],"paths":[]};
+searchIndex['horrorshow'] = {"items":[[0,"","horrorshow","# Horrorshow",null,null],[14,"xml!","","Crate a new xml template",null,null],[14,"stringify_compressed!","","",null,null],[14,"append_xml!","","Append xml to the current template.",null,null],[14,"append_raw!","","Append text without escaping.",null,null],[14,"append_fmt!","","Format, escape, and append arguments.",null,null],[14,"append!","","Escape and append text.",null,null]],"paths":[]};
 initSearch(searchIndex);
