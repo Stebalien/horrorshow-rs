a351d7ecf9081e566c149550b5bc14fec24e2e7c
Steven Allen <steven@stebalien.com>
2015-05-26 16:00:29 -0400

    Update docs for 9fa05eac2782f06c3838890cd332cb2e466a2a87
    Render by ref.
    
    Also, implement display.

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['horrorshow'] = ["impl&lt;F&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='horrorshow/struct.Renderer.html' title='horrorshow::Renderer'>Renderer</a>&lt;F&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
