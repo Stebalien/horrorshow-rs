a47b2b8e03dead7dc27065f846ec03e38fbc2e2e
Steven Allen <steven@stebalien.com>
2015-05-26 11:06:41 -0400

    Update docs for ecadb35c0a3f64fd70fa07a95acaab802af6e7dc
    errors v1.

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['horrorshow'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='horrorshow/struct.Template.html' title='horrorshow::Template'>Template</a>&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/string/struct.String.html' title='collections::string::String'>String</a>",];
+implementors['horrorshow'] = ["impl&lt;E&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;E&gt; for <a class='enum' href='horrorshow/enum.Error.html' title='horrorshow::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/trait.Shl.js
+++ implementors/core/ops/trait.Shl.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['horrorshow'] = ["impl&lt;'a, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;T&gt; for &amp;'a mut <a class='struct' href='horrorshow/struct.Template.html' title='horrorshow::Template'>Template</a>",];
+implementors['horrorshow'] = ["impl&lt;'a, 'b, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;T&gt; for &amp;'a mut <a class='struct' href='horrorshow/struct.TemplateBuilder.html' title='horrorshow::TemplateBuilder'>TemplateBuilder</a>&lt;'b&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
