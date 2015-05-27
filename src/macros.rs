8a851cfbe5d0bc90f637e1203b3b8be602895c12
Steven Allen <steven@stebalien.com>
2015-05-27 16:34:05 -0400

    Document box_html

--- src/macros.rs
+++ src/macros.rs
@@ -12,7 +12,42 @@ macro_rules! html {
     }}
 }
 
-/// Crate a new html template
+/// Crate a new owned html template.
+///
+/// This template will be boxed and will own it's environment. If you need to return a template
+/// from a function, use this.
+///
+/// Example:
+///
+/// ```rust
+/// # #[macro_use]
+/// # extern crate horrorshow;
+/// 
+/// # fn main() {
+/// fn post<'a>(title: &'a str) -> Box<horrorshow::RenderBox + 'a> {
+///     box_html! {
+///         article {
+///             title { h1 : title }
+///             p : "This is one paragraph.";
+///             p : "This is a second.";
+///         }
+///     }
+/// }
+/// println!("{}", html! {
+///     html {
+///         body {
+///             : post("First Post");
+///             |t| (0..10).fold(t, |t, i| {
+///                 // Waiting for non-lexical borrows!!!!
+///                 let tmp = format!("Spam post {}", i);
+///                 let post = post(&tmp);
+///                 t << post
+///             })
+///         }
+///     }
+/// });
+/// # }
+/// ```
 #[macro_export]
 macro_rules! box_html {
     ($($inner:tt)*) => {{
