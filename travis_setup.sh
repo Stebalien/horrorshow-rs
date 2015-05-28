97b9588cd0b08fa64c044480d57fd2cf62952e5b
Steven Allen <steven@stebalien.com>
2015-05-28 12:45:00 -0400

    Travis doesn't have sponge.

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,4 +1,4 @@
-awk '!(/# target: / && !/\<'$TRAVIS_RUST_VERSION'\>/) { print($0); }' Cargo.toml | sponge Cargo.toml
+gawk -i inplace '!(/# target: / && !/\<'$TRAVIS_RUST_VERSION'\>/) { print($0); }' Cargo.toml
 if [ ! "$TRAVIS_RUST_VERSION" = "nightly" ]; then
     rm -r benches
 fi
