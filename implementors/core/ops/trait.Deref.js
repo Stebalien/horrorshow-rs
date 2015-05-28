7633bff2663a9f1d60b78ba44e69519c6f82f4ba
Steven Allen <steven@stebalien.com>
2015-05-28 13:35:04 -0400

    Update docs for c8d31badeee40a41dcfdd78a39bec6499d6edda5
    Add trait impls for Raw.

---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['horrorshow'] = ["impl&lt;S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='horrorshow/struct.Raw.html' title='horrorshow::Raw'>Raw</a>&lt;S&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
