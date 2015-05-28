bd82a94fa93d2dcc1b24e1e1a9ad6c1e1925fa9f
Steven Allen <steven@stebalien.com>
2015-05-28 12:48:20 -0400

    travis's gawk is out of date...

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,4 +1,8 @@
-gawk -i inplace '!(/# target: / && !/\<'$TRAVIS_RUST_VERSION'\>/) { print($0); }' Cargo.toml
+#!/bin/bash
+set -e
+
+awk '!(/# target: / && !/\<'$TRAVIS_RUST_VERSION'\>/) { print($0); }' Cargo.toml > Cargo.toml.tmp
+mv Cargo.toml.tmp Cargo.toml
 if [ ! "$TRAVIS_RUST_VERSION" = "nightly" ]; then
     rm -r benches
 fi
