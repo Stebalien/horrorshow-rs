4e1503692e1447f588ae268762e34ee54d25cf50
Steven Allen <steven@stebalien.com>
2015-06-02 14:18:22 -0700

    Better benchmarks.

--- benches/horrorshow.rs
+++ benches/horrorshow.rs
@@ -7,7 +7,49 @@ extern crate horrorshow;
 use horrorshow::Template;
 
 #[bench]
-fn bench(b: &mut test::Bencher) {
+fn bench_fmt(b: &mut test::Bencher) {
+    b.iter(|| {
+        use std::fmt::Write;
+        let mut s = String::new();
+        let template = html! {
+            html {
+                head {
+                    title { : "Hello world!" }
+                }
+                body {
+                    // attributes
+                    h1(id="heading") {
+                        // Insert escaped text (actually, anything that defines Display)
+                        : "Hello! This is <html />"
+                    }
+                    // Insert raw text (unescaped)
+                    p : raw!("Let's <i>count</i> to 10!");
+                    ol(id="count") {
+                        // run some inline code...
+                        |mut tmpl| for i in 0..10 {
+                            // append to the current template.
+                            // store output because rust bug #25753
+                            tmpl = tmpl << html! {
+                                li {
+                                    // format some text
+                                    #{"{}", i+1 }
+                                }
+                            };
+                        }
+                    }
+                    // You need semi-colons for tags without children.
+                    br; br;
+                    p : "Easy!"
+                }
+            }
+        };
+        template.write_to_fmt(&mut s).unwrap();
+        test::black_box(s);
+    });
+}
+
+#[bench]
+fn bench_short(b: &mut test::Bencher) {
     b.iter(|| {
         test::black_box(html! {
             html {
@@ -25,6 +67,45 @@ fn bench(b: &mut test::Bencher) {
                     ol(id="count") {
                         // run some inline code...
                         |mut tmpl| for i in 0..10 {
+                            // append to the current template.
+                            // store output because rust bug #25753
+                            tmpl = tmpl << html! {
+                                li {
+                                    // format some text
+                                    #{"{}", i+1 }
+                                }
+                            };
+                        }
+                    }
+                    // You need semi-colons for tags without children.
+                    br; br;
+                    p : "Easy!"
+                }
+            }
+        }.into_string());
+    });
+}
+
+#[bench]
+fn bench_long(b: &mut test::Bencher) {
+    let count = test::black_box(100);
+    b.iter(|| {
+        test::black_box(html! {
+            html {
+                head {
+                    title { : "Hello world!" }
+                }
+                body {
+                    // attributes
+                    h1(id="heading") {
+                        // Insert escaped text (actually, anything that defines Display)
+                        : "Hello! This is <html />"
+                    }
+                    // Insert raw text (unescaped)
+                    p : raw!("Let's <i>count</i> to 10!");
+                    ol(id="count") {
+                        // run some inline code...
+                        |mut tmpl| for i in 0..count {
                             // append to the current template.
                             // store output because rust bug #25753
                             tmpl = tmpl << html! {
--- benches/maud.rs
+++ benches/maud.rs
@@ -5,7 +5,7 @@ extern crate test;
 extern crate maud;
 
 #[bench]
-fn bench(b: &mut test::Bencher) {
+fn bench_short(b: &mut test::Bencher) {
     b.iter(|| {
         let mut s = String::new();
         let template = html! {
@@ -41,3 +41,42 @@ fn bench(b: &mut test::Bencher) {
     });
 }
 
+
+
+#[bench]
+fn bench(b: &mut test::Bencher) {
+    let count = test::black_box(100);
+    b.iter(|| {
+        let mut s = String::new();
+        let template = html! {
+            html {
+                head {
+                    title { "Hello world!" }
+                }
+                body {
+                    h1 id="heading" {
+                        "Hello! This is <html />"
+                    }
+                    p {
+                        $$"Let's <i>count</i> to 10!"
+                    }
+                    ol id="count" {
+                        $ for i in 0..count {
+                            li {
+                                // format some text
+                                $(i+1)
+                            }
+                        }
+                    }
+                    br /
+                    br /
+                    p {
+                        "Easy!"
+                    }
+                }
+            }
+        };
+        template.render_fmt(&mut s).unwrap();
+        test::black_box(s);
+    });
+}
