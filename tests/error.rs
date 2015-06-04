2168c4bed6fedb7a45b879e79888449fd9c44cee
Steven Allen <steven@stebalien.com>
2015-06-04 15:27:22 -0400

    Test errors.

---
+++ tests/error.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate horrorshow;
+use std::error::Error;
+
+use horrorshow::Template;
+
+#[test]
+fn test_result() {
+    let mut s = String::new();
+    let e = (html! {
+        : Ok::<&str, &str>("pass");
+        : Err::<&str, &str>("fail");
+        : Ok::<&str, &str>("hidden");
+        : Err::<&str, &str>("fail2");
+    }).write_to_string(&mut s).err().unwrap();
+    assert_eq!(s, "pass");
+    assert!(e.write.is_none());
+    assert_eq!(e.render.len(), 2);
+    assert_eq!(e.render[0].description(), "fail");
+    assert_eq!(e.render[1].description(), "fail2");
+}
+
+#[test]
+fn test_record() {
+    let e = (html! {
+        tag {
+            |tmpl| tmpl.record_error("test");
+        }
+    }).into_string().err().unwrap();
+    assert!(e.write.is_none());
+    assert_eq!(e.render.len(), 1);
+    assert_eq!(e.render[0].description(), "test");
+}
