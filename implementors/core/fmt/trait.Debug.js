3dbea703574960821cf7c66ef235a2fbff4b461e
Steven Allen <steven@stebalien.com>
2015-06-04 15:19:19 -0400

    Update docs for 4deb5b5c9116cbe6e05200ad859a47053c2423e4
    Add basic error handling.

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['horrorshow'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='horrorshow/struct.Error.html' title='horrorshow::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
