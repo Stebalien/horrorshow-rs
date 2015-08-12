1beb8bb512f9c2f98081d36447db90bee4805a43
Steven Allen <steven@stebalien.com>
2015-08-12 13:16:54 -0700

    Update docs for bcfb2156cf4661f753b4c518f9129d6d756e6e96
    Fix infinite recursion

--- implementors/core/ops/trait.Deref.js
+++ implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['horrorshow'] = ["impl&lt;S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='horrorshow/struct.Raw.html' title='horrorshow::Raw'>Raw</a>&lt;S&gt;",];
+implementors['horrorshow'] = ["impl&lt;S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='horrorshow/struct.Raw.html' title='horrorshow::Raw'>Raw</a>&lt;S&gt; <span class='where'>where S: <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html' title='core::convert::AsRef'>AsRef</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt;</span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/trait.Shl.js
+++ implementors/core/ops/trait.Shl.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['horrorshow'] = ["impl&lt;'a, 'b, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;T&gt; for &amp;'a mut <a class='struct' href='horrorshow/struct.TemplateBuffer.html' title='horrorshow::TemplateBuffer'>TemplateBuffer</a>&lt;'b&gt;",];
+implementors['horrorshow'] = ["impl&lt;'a, 'b, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Shl.html' title='core::ops::Shl'>Shl</a>&lt;T&gt; for &amp;'a mut <a class='struct' href='horrorshow/struct.TemplateBuffer.html' title='horrorshow::TemplateBuffer'>TemplateBuffer</a>&lt;'b&gt; <span class='where'>where T: <a class='trait' href='horrorshow/trait.RenderOnce.html' title='horrorshow::RenderOnce'>RenderOnce</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
