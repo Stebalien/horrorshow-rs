c902e7bdfe5850393424cd4ec2c9ab67d4ece053
Steven Allen <steven@stebalien.com>
2016-01-15 13:22:10 -0500

    Update docs for f97270336aeb2d010aeb1afcb7183afbd7759959
    update maud

--- horrorshow/prelude/sidebar-items.js
+++ horrorshow/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({});
+initSidebarItems({"struct":[["FnRenderer","A template renderer. The `html! {}` macro returns a `FnRenderer`."],["TemplateBuffer","A template buffer. This is the type that gets passed to closures inside templates."]],"trait":[["Render","Something that can be rendered by reference."],["RenderBox","Something that can be rendered once out of a box."],["RenderMut","Something that can be rendered by mutable reference."],["RenderOnce","Something that can be rendered once."],["Template","A template that can be rendered into something."]]});
