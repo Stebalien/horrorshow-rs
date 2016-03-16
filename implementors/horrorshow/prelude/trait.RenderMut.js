c91e30b543479e90ab15ad63182f77bb33718521
Steven Allen <steven@stebalien.com>
2016-03-16 18:09:04 -0400

    Update docs for a1b18d36ee4049c8d614db4935efe0881da54bbd
    fix maud benchmark

--- implementors/horrorshow/prelude/trait.RenderMut.js
+++ implementors/horrorshow/prelude/trait.RenderMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['horrorshow'] = [];implementors['horrorshow'] = [];implementors['horrorshow'] = [];implementors['horrorshow'] = [];implementors['horrorshow'] = [];
+implementors['horrorshow'] = [];implementors['horrorshow'] = [];implementors['horrorshow'] = [];implementors['horrorshow'] = [];implementors['horrorshow'] = [];implementors['horrorshow'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
