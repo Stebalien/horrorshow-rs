7a3c5e4be3eb2fb3fa907703e28177ff4d4d09b0
Steven Allen <steven@stebalien.com>
2016-03-16 20:24:40 -0400

    Remove adjacent tag feature
    The implementation was broken and a correct implementation would be
    prohibitively difficult.

--- src/macros.rs
+++ src/macros.rs
@@ -241,12 +241,6 @@ macro_rules! append_html {
     ($tmpl:ident, $tag:ident : $e:expr) => {
         append_html!($tmpl, $tag { : $e; });
     };
-    ($tmpl:ident, $tag:ident($($attrs:tt)+) $tag2:ident $($next:tt)* ) => {
-        append_html!($tmpl, $tag($($attrs)*) { $tag2 $($next)* });
-    };
-    ($tmpl:ident, $tag:ident $tag2:ident $($next:tt)* ) => {
-        append_html!($tmpl, $tag { $tag2 $($next)* });
-    };
     ($tmpl:ident, $tag:ident) => {
         $tmpl.write_raw(concat!("<", stringify!($tag), "/>"));
     };
