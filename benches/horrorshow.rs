de878d92fb9a0cd38a56a41fb74806133a667c78
Steven Allen <steven@stebalien.com>
2016-10-01 22:37:39 -0700

    fix benchmarks

--- benches/horrorshow.rs
+++ benches/horrorshow.rs
@@ -9,6 +9,7 @@ use horrorshow::Template;
 #[bench]
 fn bench_short_fmt(b: &mut test::Bencher) {
     b.iter(|| {
+        let count = test::black_box(10);
         let mut s = String::new();
         let template = html! {
             html {
@@ -18,76 +19,11 @@ fn bench_short_fmt(b: &mut test::Bencher) {
                 body {
                     // attributes
                     h1(id="heading") {
-                        // Insert escaped text 
+                        // Insert escaped text
                         : "Hello! This is <html />"
                     }
                     // Insert raw text (unescaped)
-                    p : raw!("Let's <i>count</i> to 10!");
-                    ol(id="count") {
-                        // run some inline code...
-                        @ for i in 0..10 {
-                            li : format_args!("{}", i+1)
-                        }
-                    }
-                    // You need semi-colons for tags without children.
-                    br; br;
-                    p : "Easy!"
-                }
-            }
-        };
-        template.write_to_fmt(&mut s).unwrap();
-        test::black_box(s);
-    });
-}
-
-#[bench]
-fn bench_short(b: &mut test::Bencher) {
-    b.iter(|| {
-        test::black_box(html! {
-            html {
-                head {
-                    title { : "Hello world!" }
-                }
-                body {
-                    // attributes
-                    h1(id="heading") {
-                        // Insert escaped text 
-                        : "Hello! This is <html />"
-                    }
-                    // Insert raw text (unescaped)
-                    p : raw!("Let's <i>count</i> to 10!");
-                    ol(id="count") {
-                        // run some inline code...
-                        @ for i in 0..10 {
-                            li : format_args!("{}", i+1)
-                        }
-                    }
-                    // You need semi-colons for tags without children.
-                    br; br;
-                    p : "Easy!"
-                }
-            }
-        }.into_string().unwrap());
-    });
-}
-
-#[bench]
-fn bench_long(b: &mut test::Bencher) {
-    let count = test::black_box(100);
-    b.iter(|| {
-        test::black_box(html! {
-            html {
-                head {
-                    title { : "Hello world!" }
-                }
-                body {
-                    // attributes
-                    h1(id="heading") {
-                        // Insert escaped text 
-                        : "Hello! This is <html />"
-                    }
-                    // Insert raw text (unescaped)
-                    p : raw!("Let's <i>count</i> to 100!");
+                    p : raw!("Let's <i>count</i>!");
                     ol(id="count") {
                         // run some inline code...
                         @ for i in 0..count {
@@ -99,6 +35,71 @@ fn bench_long(b: &mut test::Bencher) {
                     p : "Easy!"
                 }
             }
-        }.into_string().unwrap());
+        };
+        template.write_to_fmt(&mut s).unwrap()
+    });
+}
+
+#[bench]
+fn bench_short(b: &mut test::Bencher) {
+    b.iter(|| {
+        let count = test::black_box(10);
+        (html! {
+            html {
+                head {
+                    title { : "Hello world!" }
+                }
+                body {
+                    // attributes
+                    h1(id="heading") {
+                        // Insert escaped text
+                        : "Hello! This is <html />"
+                    }
+                    // Insert raw text (unescaped)
+                    p : raw!("Let's <i>count</i>!");
+                    ol(id="count") {
+                        // run some inline code...
+                        @ for i in 0..count {
+                            li : format_args!("{}", i+1)
+                        }
+                    }
+                    // You need semi-colons for tags without children.
+                    br; br;
+                    p : "Easy!"
+                }
+            }
+        }).into_string().unwrap()
+    });
+}
+
+#[bench]
+fn bench_long(b: &mut test::Bencher) {
+    b.iter(|| {
+        let count = test::black_box(100);
+        (html! {
+            html {
+                head {
+                    title { : "Hello world!" }
+                }
+                body {
+                    // attributes
+                    h1(id="heading") {
+                        // Insert escaped text
+                        : "Hello! This is <html />"
+                    }
+                    // Insert raw text (unescaped)
+                    p : raw!("Let's <i>count</i>!");
+                    ol(id="count") {
+                        // run some inline code...
+                        @ for i in 0..count {
+                            li : format_args!("{}", i+1)
+                        }
+                    }
+                    // You need semi-colons for tags without children.
+                    br; br;
+                    p : "Easy!"
+                }
+            }
+        }).into_string().unwrap()
     });
 }
--- benches/maud.rs
+++ benches/maud.rs
@@ -6,39 +6,41 @@ extern crate maud;
 
 use maud::PreEscaped;
 
-#[bench]
-fn bench_short(b: &mut test::Bencher) {
-    b.iter(|| {
-        let mut s = String::new();
-        html!(s, {
-            html {
-                head {
-                    title { "Hello world!" }
+fn count(count: u32) -> String {
+    (html!{
+        html {
+            head {
+                title { "Hello world!" }
+            }
+            body {
+                h1 id="heading" {
+                    "Hello! This is <html />"
                 }
-                body {
-                    h1 id="heading" {
-                        "Hello! This is <html />"
-                    }
-                    p {
-                        ^PreEscaped("Let's <i>count</i> to 10!")
-                    }
-                    ol id="count" {
-                        @for i in 0..10 {
-                            li {
-                                // format some text
-                                ^(i+1)
-                            }
+                p {
+                    (PreEscaped("Let's <i>count</i>!"))
+                }
+                ol id="count" {
+                    @for i in 0..count {
+                        li {
+                            // format some text
+                            (i+1)
                         }
                     }
-                    br /
+                }
+                br /
                     br /
                     p {
                         "Easy!"
                     }
-                }
             }
-        }).unwrap();
-        test::black_box(s);
+        }
+    }).into_string()
+}
+
+#[bench]
+fn bench_short(b: &mut test::Bencher) {
+    b.iter(|| {
+        count(test::black_box(10))
     });
 }
 
@@ -46,37 +48,7 @@ fn bench_short(b: &mut test::Bencher) {
 
 #[bench]
 fn bench_long(b: &mut test::Bencher) {
-    let count = test::black_box(100);
     b.iter(|| {
-        let mut s = String::new();
-        html!(s, {
-            html {
-                head {
-                    title { "Hello world!" }
-                }
-                body {
-                    h1 id="heading" {
-                        "Hello! This is <html />"
-                    }
-                    p {
-                        ^PreEscaped("Let's <i>count</i> to 100!")
-                    }
-                    ol id="count" {
-                        @for i in 0..count {
-                            li {
-                                // format some text
-                                ^(i+1)
-                            }
-                        }
-                    }
-                    br /
-                    br /
-                    p {
-                        "Easy!"
-                    }
-                }
-            }
-        }).unwrap();
-        test::black_box(s);
+        count(test::black_box(100))
     });
 }
