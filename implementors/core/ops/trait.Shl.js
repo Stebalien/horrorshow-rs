1c03bc3047781578359146e61e25e1974f33424d
Steven Allen <steven@stebalien.com>
2017-04-28 11:33:02 -0700

    Update docs for 4563edcc5e0115092e3e83187344ba8b74aab365
    Bump to 0.6.2

--- implementors/core/ops/trait.Shl.js
+++ implementors/core/ops/trait.Shl.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["horrorshow"] = ["impl&lt;'a, 'b, T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/ops/trait.Shl.html" title="trait core::ops::Shl">Shl</a>&lt;T&gt; for &amp;'a mut <a class="struct" href="horrorshow/struct.TemplateBuffer.html" title="struct horrorshow::TemplateBuffer">TemplateBuffer</a>&lt;'b&gt; <span class="where fmt-newline">where T: <a class="trait" href="horrorshow/prelude/trait.RenderOnce.html" title="trait horrorshow::prelude::RenderOnce">RenderOnce</a></span>",];
+implementors["horrorshow"] = ["impl&lt;'a, 'b, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/trait.Shl.html\" title=\"trait core::ops::Shl\">Shl</a>&lt;T&gt; for &amp;'a mut <a class=\"struct\" href=\"horrorshow/struct.TemplateBuffer.html\" title=\"struct horrorshow::TemplateBuffer\">TemplateBuffer</a>&lt;'b&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"horrorshow/prelude/trait.RenderOnce.html\" title=\"trait horrorshow::prelude::RenderOnce\">RenderOnce</a>,&nbsp;</span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
