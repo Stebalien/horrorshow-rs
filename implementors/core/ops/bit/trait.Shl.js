4d1cc88ea0f5036b8c2686795c042ac08ee83578
Steven Allen <steven@stebalien.com>
2017-10-06 11:52:04 +0300

    Update docs for 413ab07b6731ad77c1bcfc87d58376869d24551d
    fix maud

---
+++ implementors/core/ops/bit/trait.Shl.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["horrorshow"] = ["impl&lt;'a, 'b, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Shl.html\" title=\"trait core::ops::bit::Shl\">Shl</a>&lt;T&gt; for &amp;'a mut <a class=\"struct\" href=\"horrorshow/struct.TemplateBuffer.html\" title=\"struct horrorshow::TemplateBuffer\">TemplateBuffer</a>&lt;'b&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"horrorshow/prelude/trait.RenderOnce.html\" title=\"trait horrorshow::prelude::RenderOnce\">RenderOnce</a>,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
