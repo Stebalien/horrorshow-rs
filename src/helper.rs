900df0ffb4cb5b7affab44c5577fc94ea6a7cb2c
Steven Allen <steven@stebalien.com>
2019-03-16 11:24:33 -0700

    fix doc comments for doctypes

--- src/helper.rs
+++ src/helper.rs
@@ -1,7 +1,8 @@
 //! Helpers templates.
 
 macro_rules! c {
-    ($name:ident $value:tt) => {
+    ($doc:expr => $name:ident $value:tt) => {
+        #[doc($doc)]
         #[allow(non_upper_case_globals)]
         pub const $name: $crate::render::Raw<&'static str> = $crate::render::Raw($value);
     };
@@ -9,19 +10,18 @@ macro_rules! c {
 
 /// Helpers for inserting `DOCTYPE`s.
 pub mod doctype {
-    /// The standard HTML5 doctype (this *is* the doctype you're looking for).
-    c!(HTML "<!DOCTYPE html>");
+    c!("The standard HTML5 doctype (this *is* the doctype you're looking for)." => HTML "<!DOCTYPE html>");
 
     /// HTML 4.01 doctypes
     #[allow(non_snake_case)]
     pub mod HTML4 {
-        /// The HTML 4.001 strict doctype.
-        c!(Strict "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
+        c!("The HTML 4.001 strict doctype." =>
+           Strict "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
 
-        /// The HTML 4.001 transitional doctype.
-        c!(Transitional "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">");
+        c!("The HTML 4.001 transitional doctype." =>
+           Transitional "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">");
 
-        /// The HTML 4.001 frameset doctype.
-        c!(Frameset "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\">");
+        c!("The HTML 4.001 frameset doctype." =>
+           Frameset "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\">");
     }
 }
