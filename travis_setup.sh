368122e22d48f16a2173b131776b267a75d181aa
Steven Allen <steven@stebalien.com>
2023-04-18 08:28:32 -0700

    chore: add github CI (#47)
    Travis is dead.

--- travis_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-awk '!(/# target: / && !/\<'$TRAVIS_RUST_VERSION'\>/) { print($0); }' Cargo.toml > Cargo.toml.tmp
-mv Cargo.toml.tmp Cargo.toml
-if [ ! "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-    rm -r benches
-fi
